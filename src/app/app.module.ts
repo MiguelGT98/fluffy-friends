8690b19e18aece53c9e9368f63956e3233adb18c
Miguel Gallardo <jgallardo9814@gmail.com>
2020-04-26 17:51:46 -0500

    Fix typo on home component import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { FooterComponent } from './common/footer/footer.component';
-import { HomeComponent } from './modules/landingPage/home/home.component';
+import { HomeComponent } from './modules/landing-page/home/home.component';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, FooterComponent, HomeComponent],
