79c9d4da2c3758f6e0fcecd477ef99ad17d208c8
jgenel <jgenel@gmail.com>
2020-04-27 16:06:32 -0500

    corregido puse 2 componentes

---
+++ src/app/modules/dog-friends/components/dog-friends/dog-friends.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-friends',
+  templateUrl: './dog-friends.component.html',
+  styleUrls: ['./dog-friends.component.scss']
+})
+export class DogFriendsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/dog-friends/dog-friends.module.ts
+++ src/app/modules/dog-friends/dog-friends.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DogMatchesComponent } from './dog-matches/dog-matches.component';
+import { DogMatchesComponent } from './components/dog-matches/dog-matches.component';
 import { AppModule } from 'src/app/app.module';
+import { DogFriendsComponent } from './components/dog-friends/dog-friends.component';
 
 
 @NgModule({
-  declarations: [DogMatchesComponent],
+  declarations: [DogMatchesComponent, DogFriendsComponent],
   imports: [CommonModule, AppModule]
 })
 export class DogFriendsModule { }
