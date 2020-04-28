9d1bebd0d95019f40952493a5884a4c1b01d551b
Miguel Gallardo <jgallardo9814@gmail.com>
2020-04-27 20:09:41 -0500

    Created Add and Edit components with stories.

--- src/app/modules/dog-friends/dog-friends.module.ts
+++ src/app/modules/dog-friends/dog-friends.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppModule } from 'src/app/app.module';
 import { DogFriendsComponent } from './components/dog-friends/dog-friends.component';
+import { NewFriendComponent } from './components/new-friend/new-friend.component';
+import { EditFriendComponent } from './components/edit-friend/edit-friend.component';
 
 @NgModule({
-  declarations: [DogFriendsComponent],
+  declarations: [DogFriendsComponent, NewFriendComponent, EditFriendComponent],
   imports: [CommonModule, AppModule],
 })
 export class DogFriendsModule {}
--- src/stories/Friends.stories.ts
+++ src/stories/Friends.stories.ts
@@ -2,6 +2,8 @@ import { moduleMetadata, storiesOf } from '@storybook/angular';
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css';
 import { DogFriendsModule } from 'src/app/modules/dog-friends/dog-friends.module';
 import { DogFriendsComponent } from 'src/app/modules/dog-friends/components/dog-friends/dog-friends.component';
+import { NewFriendComponent } from 'src/app/modules/dog-friends/components/new-friend/new-friend.component';
+import { EditFriendComponent } from 'src/app/modules/dog-friends/components/edit-friend/edit-friend.component';
 
 export default { title: 'Friends' };
 
@@ -9,4 +11,10 @@ storiesOf('Friends', module)
   .addDecorator(moduleMetadata({ imports: [DogFriendsModule] }))
   .add('Friends table', () => ({
     component: DogFriendsComponent,
+  }))
+  .add('New friend', () => ({
+    component: NewFriendComponent,
+  }))
+  .add('Edit friend', () => ({
+    component: EditFriendComponent,
   }));
