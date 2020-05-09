1d62ef2e548c4b46edb9b4faab5d7591e4e2cc7f
Miguel Gallardo <jgallardo9814@gmail.com>
2020-05-09 11:32:13 -0500

    Get friend from localstorage on edit component.

--- src/app/modules/dog-friends/components/edit-friend/edit-friend.component.ts
+++ src/app/modules/dog-friends/components/edit-friend/edit-friend.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendsLocalStorageService } from '../../services/friends-local-storage.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-friend',
   templateUrl: './edit-friend.component.html',
-  styleUrls: ['./edit-friend.component.scss']
+  styleUrls: ['./edit-friend.component.scss'],
 })
 export class EditFriendComponent implements OnInit {
+  id = '';
+  friend: any;
 
-  constructor() { }
+  constructor(
+    private friendsLocalStorageService: FriendsLocalStorageService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+      this.getFriend(this.id);
+    });
   }
 
+  getFriend(id: string) {
+    this.friend = this.friendsLocalStorageService.getFriend(id);
+  }
+
+  updateFriend(friend: any, id: string) {
+    this.friendsLocalStorageService.updateFriend(friend, id);
+  }
 }
--- src/app/modules/dog-tinder/components/dog-card/dog-card.component.ts
+++ src/app/modules/dog-tinder/components/dog-card/dog-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dog-card',
   templateUrl: './dog-card.component.html',
-  styleUrls: ['./dog-card.component.scss']
+  styleUrls: ['./dog-card.component.scss'],
 })
 export class DogCardComponent implements OnInit {
+  @Input() url: string;
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
